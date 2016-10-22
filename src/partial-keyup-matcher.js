800dc79dcbb46b57c25d3421627771b67a0f6107
Ian Olsen <iolsen@github.com>
2016-10-21 18:00:13 -0700

    PartialKeyupMatcher basically works, has tests

--- src/partial-keyup-matcher.js
+++ src/partial-keyup-matcher.js
@@ -1,7 +1,7 @@
 'use babel'
 
 module.exports =
-class PartialKeyupMatcher { // Ian TODO this name sucks
+class PartialKeyupMatcher {
 
   constructor () {
     this._pendingMatches = new Set()
@@ -12,24 +12,51 @@ class PartialKeyupMatcher { // Ian TODO this name sucks
     keyBinding['nextKeyUpIndex'] = 0
   }
 
-  // Returns matching bindings(s) if any.
+  // Returns matching bindingss, if any.
   // Updates state for next match.
-  getMatches (keyupKeystroke) {
-    let matches = []
-    for (let keyBinding of this._pendingMatches) {
-      let toMatch = keyBinding.getKeyups()[keyBinding['nextKeyUpIndex']]
-      if (keyupKeystroke === toMatch) {
-        if (keyBinding['nextKeyUpIndex'] === keyBinding.getKeyups().length - 1) {
-          // Full match. Remove and return it.
-          this._pendingMatches.delete(keyBinding)
-          matches.push(keyBinding)
-        } else {
-          // Partial match. Increment what we're looking for next.
-          keyBinding['nextKeyUpIndex']++
+  getMatches (userKeyupKeystroke) {
+    userKeyupKeystroke = this._normalizeKeystroke(userKeyupKeystroke)
+    let matches = new Set()
+
+    // Loop over each pending keyup match.
+    for (const keyBinding of this._pendingMatches) {
+      const bindingKeystrokeToMatch = this._normalizeKeystroke(keyBinding.getKeyups()[keyBinding['nextKeyUpIndex']])
+      const userKeyups = userKeyupKeystroke.split('-')
+
+      // Attempt to match multi-keyup combinations e.g. ^ctrl-shift
+      if (userKeyups.length > 1) {
+        if (userKeyupKeystroke === bindingKeystrokeToMatch) {
+          this._updateStateForMatch(matches, keyBinding)
+        }
+      }
+
+      // Loop over individual keys in the user keystroke because we want e.g.
+      // user keystroke ^ctrl-shift to match a pending ^ctrl or ^shift.
+      for (const userKeyup of userKeyups) {
+        if (userKeyup === bindingKeystrokeToMatch) {
+          this._updateStateForMatch(matches, keyBinding)
         }
       }
     }
-    return matches
+    return [...matches]
+  }
+
+  /** Private Section **/
+
+  _normalizeKeystroke (keystroke) {
+    if (keystroke[0] === '^') return keystroke.substring(1)
+    return keystroke
+  }
+
+  _updateStateForMatch (matches, keyBinding) {
+    if (keyBinding['nextKeyUpIndex'] === keyBinding.getKeyups().length - 1) {
+      // Full match. Remove and return it.
+      this._pendingMatches.delete(keyBinding)
+      matches.add(keyBinding)
+    } else {
+      // Partial match. Increment what we're looking for next.
+      keyBinding['nextKeyUpIndex']++
+    }
   }
 
 }
