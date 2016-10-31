b181539d4a54b918f3e2563b06e7bca0d7567db1
Dick Choi <fluke8259@gmail.com>
2016-11-01 06:33:23 +0900

    convert src/us-keymap.coffee

---
+++ src/us-keymap.js
@@ -0,0 +1,254 @@
+module.exports = {
+  'Space': {
+    'unmodified': ' ',
+    'withShift': ' '
+  },
+  'Digit0': {
+    'unmodified': '0',
+    'withShift': ')'
+  },
+  'Digit1': {
+    'unmodified': '1',
+    'withShift': '!'
+  },
+  'Digit2': {
+    'unmodified': '2',
+    'withShift': '@'
+  },
+  'Digit3': {
+    'unmodified': '3',
+    'withShift': '#'
+  },
+  'Digit4': {
+    'unmodified': '4',
+    'withShift': '$'
+  },
+  'Digit5': {
+    'unmodified': '5',
+    'withShift': '%'
+  },
+  'Digit6': {
+    'unmodified': '6',
+    'withShift': '^'
+  },
+  'Digit7': {
+    'unmodified': '7',
+    'withShift': '&'
+  },
+  'Digit8': {
+    'unmodified': '8',
+    'withShift': '*'
+  },
+  'Digit9': {
+    'unmodified': '9',
+    'withShift': '('
+  },
+  'KeyA': {
+    'unmodified': 'a',
+    'withShift': 'A'
+  },
+  'KeyB': {
+    'unmodified': 'b',
+    'withShift': 'B'
+  },
+  'KeyC': {
+    'unmodified': 'c',
+    'withShift': 'C'
+  },
+  'KeyD': {
+    'unmodified': 'd',
+    'withShift': 'D'
+  },
+  'KeyE': {
+    'unmodified': 'e',
+    'withShift': 'E'
+  },
+  'KeyF': {
+    'unmodified': 'f',
+    'withShift': 'F'
+  },
+  'KeyG': {
+    'unmodified': 'g',
+    'withShift': 'G'
+  },
+  'KeyH': {
+    'unmodified': 'h',
+    'withShift': 'H'
+  },
+  'KeyI': {
+    'unmodified': 'i',
+    'withShift': 'I'
+  },
+  'KeyJ': {
+    'unmodified': 'j',
+    'withShift': 'J'
+  },
+  'KeyK': {
+    'unmodified': 'k',
+    'withShift': 'K'
+  },
+  'KeyL': {
+    'unmodified': 'l',
+    'withShift': 'L'
+  },
+  'KeyM': {
+    'unmodified': 'm',
+    'withShift': 'M'
+  },
+  'KeyN': {
+    'unmodified': 'n',
+    'withShift': 'N'
+  },
+  'KeyO': {
+    'unmodified': 'o',
+    'withShift': 'O'
+  },
+  'KeyP': {
+    'unmodified': 'p',
+    'withShift': 'P'
+  },
+  'KeyQ': {
+    'unmodified': 'q',
+    'withShift': 'Q'
+  },
+  'KeyR': {
+    'unmodified': 'r',
+    'withShift': 'R'
+  },
+  'KeyS': {
+    'unmodified': 's',
+    'withShift': 'S'
+  },
+  'KeyT': {
+    'unmodified': 't',
+    'withShift': 'T'
+  },
+  'KeyU': {
+    'unmodified': 'u',
+    'withShift': 'U'
+  },
+  'KeyV': {
+    'unmodified': 'v',
+    'withShift': 'V'
+  },
+  'KeyW': {
+    'unmodified': 'w',
+    'withShift': 'W'
+  },
+  'KeyX': {
+    'unmodified': 'x',
+    'withShift': 'X'
+  },
+  'KeyY': {
+    'unmodified': 'y',
+    'withShift': 'Y'
+  },
+  'KeyZ': {
+    'unmodified': 'z',
+    'withShift': 'Z'
+  },
+  'Numpad0': {
+    'unmodified': '0',
+    'withShift': '0'
+  },
+  'Numpad1': {
+    'unmodified': '1',
+    'withShift': '1'
+  },
+  'Numpad2': {
+    'unmodified': '2',
+    'withShift': '2'
+  },
+  'Numpad3': {
+    'unmodified': '3',
+    'withShift': '3'
+  },
+  'Numpad4': {
+    'unmodified': '4',
+    'withShift': '4'
+  },
+  'Numpad5': {
+    'unmodified': '5',
+    'withShift': '5'
+  },
+  'Numpad6': {
+    'unmodified': '6',
+    'withShift': '6'
+  },
+  'Numpad7': {
+    'unmodified': '7',
+    'withShift': '7'
+  },
+  'Numpad8': {
+    'unmodified': '8',
+    'withShift': '8'
+  },
+  'Numpad9': {
+    'unmodified': '9',
+    'withShift': '9'
+  },
+  'NumpadMultiply': {
+    'unmodified': '*',
+    'withShift': '*'
+  },
+  'NumpadAdd': {
+    'unmodified': '+',
+    'withShift': '+'
+  },
+  'NumpadSubtract': {
+    'unmodified': '-',
+    'withShift': '-'
+  },
+  'NumpadDecimal': {
+    'unmodified': '.',
+    'withShift': '.'
+  },
+  'NumpadDivide': {
+    'unmodified': '/',
+    'withShift': '/'
+  },
+  'Semicolon': {
+    'unmodified': ';',
+    'withShift': ':'
+  },
+  'Equal': {
+    'unmodified': '=',
+    'withShift': '+'
+  },
+  'NumpadComma': {
+    'unmodified': ',',
+    'withShift': '<'
+  },
+  'Minus': {
+    'unmodified': '-',
+    'withShift': '_'
+  },
+  'Period': {
+    'unmodified': '.',
+    'withShift': '>'
+  },
+  'Slash': {
+    'unmodified': '/',
+    'withShift': '?'
+  },
+  'Backquote': {
+    'unmodified': '`',
+    'withShift': '~'
+  },
+  'BracketLeft': {
+    'unmodified': '[',
+    'withShift': '{'
+  },
+  'IntlYen': {
+    'unmodified': '\\',
+    'withShift': '|'
+  },
+  'BracketRight': {
+    'unmodified': ']',
+    'withShift': '}'
+  },
+  'Quote': {
+    'unmodified': "'",
+    'withShift': '"'
+  }
+}
